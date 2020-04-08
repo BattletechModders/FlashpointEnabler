c09048de2926da17d9468fcf87eda61f51722a8d
Jamie Wolf <siskot@hotmail.com>
2020-04-07 22:03:21 -0400

    Update based on morph's old changes

--- RepeatableFlashpoints/RepeatableFlashpoints/Patch.cs
+++ RepeatableFlashpoints/RepeatableFlashpoints/Patch.cs
@@ -48,56 +48,96 @@ namespace RepeatableFlashpoints {
     }
 
     [HarmonyPatch(typeof(SimGameState), "ApplyEventAction")]
-    public static class SimGameState_ApplyEventAction_Patch {
-        private static void Prefix(SimGameState __instance, ref SimGameResultAction action) {
-            try {
-                if (action.Type == SimGameResultAction.ActionType.Flashpoint_AddContract || action.Type == SimGameResultAction.ActionType.Flashpoint_StartContract) {
+    public static class SimGameState_ApplyEventAction_Patch
+    {
+        private static void Prefix(SimGameState __instance, ref SimGameResultAction action)
+        {
+            try
+            {
+                if (action.Type == SimGameResultAction.ActionType.Flashpoint_AddContract || action.Type == SimGameResultAction.ActionType.Flashpoint_StartContract)
+                {
                     FlashpointEnabler.Logger.LogLine("FP name = " + action.additionalValues[2]);
                     SimGameState simulation = UnityGameInstance.BattleTechGame.Simulation;
-                    if (action.additionalValues[3].Equals("{RANDOM}")) {
-                        List<FactionValue> values = FactionEnumeration.FactionList;
-                        Random random = new Random();
-                        FactionValue randomFaction;
-                        do {
-                            randomFaction = (FactionValue)values[(random.Next(values.Count))];
-                        }
-                        while (Helper.IsExcluded(randomFaction.Name));
-                        action.additionalValues[3] = randomFaction.Name;
+                    
+                    //Employer
+                    if (action.additionalValues[3].Equals("{RANDOM}"))
+                    {
+                        action.additionalValues[3] = generateRandomFactionString();
                         FlashpointEnabler.Logger.LogLine("Selected Faction = " + action.additionalValues[3]);
                     }
-                    else if (action.additionalValues[3].Equals("{PLANETOWNER}")) {
+                    else if (action.additionalValues[3].Equals("{PLANETOWNER}"))
+                    {
                         action.additionalValues[3] = simulation.ActiveFlashpoint.CurSystem.OwnerValue.Name;
                         FlashpointEnabler.Logger.LogLine("Set System owner Faction = " + action.additionalValues[3]);
                     }
-                    if (action.additionalValues[4].Equals("{RANDOM}")) {
-                        List<FactionValue> values = FactionEnumeration.FactionList;
-                        Random random = new Random();
-                        FactionValue randomFaction;
-                        do
-                        {
-                            randomFaction = (FactionValue)values[(random.Next(values.Count))];
-                        }
-                        while (Helper.IsExcluded(randomFaction.Name));
-                        action.additionalValues[4] = randomFaction.Name;
+                    else if (action.additionalValues[3].Equals("{ACTIVE}"))
+                    {
+                        action.additionalValues[3] = generateActiveFactionString(simulation);
+                    }
+
+                    //Target
+                    if (action.additionalValues[4].Equals("{RANDOM}"))
+                    {
+                        action.additionalValues[4] = generateRandomFactionString();
                         FlashpointEnabler.Logger.LogLine("Selected Faction = " + action.additionalValues[4]);
                     }
-                    else if (action.additionalValues[4].Equals("{PLANETOWNER}")) {
+                    else if (action.additionalValues[4].Equals("{PLANETOWNER}"))
+                    {
                         action.additionalValues[4] = simulation.ActiveFlashpoint.CurSystem.OwnerValue.Name;
                         FlashpointEnabler.Logger.LogLine("Set System owner Faction = " + action.additionalValues[4]);
                     }
-                    if (string.IsNullOrEmpty(action.value)) {
+                    else if (action.additionalValues[4].Equals("{ACTIVE}"))
+                    {
+                        action.additionalValues[4] = generateActiveFactionString(simulation);
+                    }
+                    if (string.IsNullOrEmpty(action.value))
+                    {
                         ContractOverride contractOverride = simulation.DataManager.ContractOverrides.Get(action.additionalValues[2]).Copy();
                         int contractType = contractOverride.ContractTypeValue.ID;
-                       List<MapAndEncounters> releasedMapsAndEncountersByContractTypeAndOwnership = MetadataDatabase.Instance.GetReleasedMapsAndEncountersByContractTypeAndOwnership(contractType, false);
+                        List<MapAndEncounters> releasedMapsAndEncountersByContractTypeAndOwnership = MetadataDatabase.Instance.GetReleasedMapsAndEncountersByContractTypeAndOwnership(contractType, false);
+                        foreach (MapAndEncounters map in releasedMapsAndEncountersByContractTypeAndOwnership)
+                        {
+                            if (map.Map.MapName.Equals("mapGeneral_terraceLakes_vLow"))
+                            {
+                                releasedMapsAndEncountersByContractTypeAndOwnership.Remove(map);
+                            }
+                        }
                         releasedMapsAndEncountersByContractTypeAndOwnership.Shuffle();
                         MapAndEncounters mapAndEncounters = releasedMapsAndEncountersByContractTypeAndOwnership[0];
                         action.value = mapAndEncounters.Map.MapName;
                     }
                 }
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 FlashpointEnabler.Logger.LogError(e);
             }
+        }
+
+        private static string generateRandomFactionString()
+        {
+            List<FactionValue> values = FactionEnumeration.FactionList;
+            Random random = new Random();
+            FactionValue randomFaction;
+            do
+            {
+                randomFaction = (FactionValue)values[(random.Next(values.Count))];
+            }
+            while (Helper.IsExcluded(randomFaction.Name));
+            return randomFaction.Name;
+        }
+
+        private static string generateActiveFactionString(SimGameState simulation)
+        {
+            List<string> values = simulation.ActiveFlashpoint.CurSystem.Def.ContractEmployerIDList;
+            Random random = new Random();
+            string randomFaction;
+            do
+            {
+                randomFaction = (string)values[(random.Next(values.Count))];
+            }
+            while (Helper.IsExcluded(randomFaction) || randomFaction == simulation.ActiveFlashpoint.CurSystem.OwnerValue.Name);
+            return randomFaction;
         }
     }
 
